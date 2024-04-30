c0aff2cf837b0b7153a7ead1d243eb392c5ce77a
KaMaDi <artkamadi@gmail.com>
2024-04-30 11:11:56 +0300

    сделал новый экран

---
+++ QuizQuestions/Controllers/AddNewQuestionsController.swift
@@ -0,0 +1,21 @@
+//
+//  AddNewQuestionsController.swift
+//  QuizQuestions
+//
+//  Created by Jedi on 30.04.2024.
+//
+
+import UIKit
+
+class AddNewQuestionsController: UIViewController {
+var mainView = NewQuestionView()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view = mainView
+     
+    }
+    
+
+}
